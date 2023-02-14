05cf62b0c447a581d6e2ec32afa6356db7ae74a3
pacoromo <pacoromo@me.com>
2023-02-14 14:30:49 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h2>Paco Eres Cabron!</h2>
+      <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
