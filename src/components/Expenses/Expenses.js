71040eabc6a8176ccb601a063530d864a75f4cae
pacoromo <pacoromo@me.com>
2023-02-22 17:31:55 -0500

    improved composition

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
+import ExpensesList from "./ExpensesList";
 import "./Expenses.css";
 
 const Expenses = ({ items }) => {
@@ -15,26 +15,13 @@ const Expenses = ({ items }) => {
         (item) => item.date.getFullYear() === +yearSelected
     );
 
-    let expensesContent = <p>No expenses found.</p>;
-
-    if (filteredItems.length > 0) {
-        expensesContent = filteredItems.map((item) => (
-            <ExpenseItem
-                key={item.id}
-                title={item.title}
-                amount={item.amount}
-                date={item.date}
-            />
-        ));
-    }
-
     return (
         <Card className="expenses">
             <ExpensesFilter
                 selected={yearSelected}
                 onYearSelection={yearSelectionHandler}
             />
-            {expensesContent}
+            <ExpensesList items={filteredItems} />
 
             {/* {filteredItems.length === 0 && <p>No expenses found.</p>}
             {filteredItems.length > 0 &&
